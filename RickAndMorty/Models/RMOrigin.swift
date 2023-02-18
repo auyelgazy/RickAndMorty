29075f1d30debabd4240f0c358ecfcc1eac517e3
Kuanysh Auyelgazy <auyelgazy@gmail.com>
2023-02-18 16:56:19 +0600

    Created data models.

---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,13 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by Kuanysh al-Khattab Auyelgazy on 18.02.2023.
+//
+
+import Foundation
+
+struct RMOrigin: Codable {
+    let name: String
+    let url: String
+}
